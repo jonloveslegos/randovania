269fd2694e7af7519be635b6fdb0f34460845f65
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-01-28 21:05:36 +0200

    extra logging in generation failure

--- randovania/generator/filler/player_state.py
+++ randovania/generator/filler/player_state.py
@@ -144,7 +144,17 @@ class PlayerState:
                        for resource in interesting_resources_for_reach(self.reach)
                        if resource.resource_type == ResourceType.ITEM}
 
-        return ("At {0} after {1} actions and {2} pickups, with {3} collected locations.\n\n"
+        for node in self.reach.all_nodes:
+            if not (self.reach.is_reachable_node(node) or node in self.reach.connected_nodes):
+                continue
+            print("{} - {} - {}".format(
+                self.game.world_list.node_name(node),
+                self.reach.is_reachable_node(node),
+                self.reach.is_safe_node(node),
+            ))
+
+        return ("At {0} after {1} actions and {2} pickups, with {3} collected locations,"
+                " {6} nodes in reach and {7} safe nodes.\n\n"
                 "Pickups still available: {4}\n\nResources to progress: {5}").format(
             self.game.world_list.node_name(self.reach.state.node, with_world=True, distinguish_dark_aether=True),
             self.num_actions,
@@ -153,6 +163,8 @@ class PlayerState:
             ", ".join(name if quantity == 1 else f"{name} x{quantity}"
                       for name, quantity in sorted(pickups_by_name_and_quantity.items())),
             ", ".join(sorted(to_progress)),
+            sum(1 for n in self.reach.all_nodes if self.reach.is_reachable_node(n)),
+            sum(1 for n in self.reach.all_nodes if self.reach.is_safe_node(n)),
         )
 
 
